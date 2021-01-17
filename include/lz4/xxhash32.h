257fedd4deba1b7de8c5d9a8c0145e1e448d3057
AVG <avgx64@yahoo.com>
2021-01-17 19:20:08 +0100

    trunk4 17Jan2021

--- include/lz4/xxhash32.h
+++ include/lz4/xxhash32.h
@@ -1,19 +1,19 @@
 #pragma once
 
+#ifndef XXH_H32_79
+#defune XXH_H32_79
 
+#ifdef (__cplusplus)
+	extern "C" {
+#endif
 
+// TODO...	
 
+#ifdef (__cplusplus)
+	}
+#endif
 
-
-
-
-
-
-
-
-
-
-
+#endif
 
 
 
